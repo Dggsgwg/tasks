10f5e10e9bdb645c82a2da188c7ecc3402c38cb1
Dggsgwg <danilvad882@gmail.com>
2021-10-26 15:43:59 +0300

    finished menu,debugged task 20

--- src/Mine.java
+++ src/Mine.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Mine {
     static Scanner scanner = new Scanner(System.in);
     static DecimalFormat decimalFormat = new DecimalFormat("#.###");
+    static String command = "skip";
+    static int temp = 0;
 
     public static void task20() {
         int hours = 0;
@@ -18,48 +20,48 @@ public class Mine {
             if (hours == 1 | hours == 21) {
                 switch (minutes) {
                     case 0:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час", hours);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час\n", hours);
                         break;
                     case 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 26, 27, 28, 29, 30, 35, 36, 37, 38, 39, 40, 45, 46, 47, 48, 49, 50, 55, 56:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час и %d минут", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час и %d минут\n", hours, minutes);
                         break;
                     case 21, 31, 41, 51:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час и %d минуту", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час и %d минуту\n", hours, minutes);
                         break;
                     case 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час и %d минуты", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d час и %d минуты\n", hours, minutes);
                         break;
                 }
             }
             if (hours == 2 | hours == 3 | hours == 4 | hours == 22 | hours == 23 | hours == 24) {
                 switch (minutes) {
                     case 0:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа", hours);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа\n", hours);
                         break;
                     case 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 26, 27, 28, 29, 30, 35, 36, 37, 38, 39, 40, 45, 46, 47, 48, 49, 50, 55, 56:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа и %d минут", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа и %d минут\n", hours, minutes);
                         break;
                     case 21, 31, 41, 51:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа и %d минуту", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа и %d минуту\n", hours, minutes);
                         break;
                     case 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа и %d минуты", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часа и %d минуты\n", hours, minutes);
                         break;
                 }
             }
-            if(hours == 0 | hours >=5 & hours<=20) {
+            if (hours == 0 | hours >= 5 & hours <= 20) {
                 switch (minutes) {
                     case 0:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов", hours);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов\n", hours);
                         break;
                     case 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 26, 27, 28, 29, 30, 35, 36, 37, 38, 39, 40, 45, 46, 47, 48, 49, 50, 55, 56:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов и %d минут", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов и %d минут\n", hours, minutes);
                         break;
                     case 21, 31, 41, 51:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов и %d минуту", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов и %d минуту\n", hours, minutes);
                         break;
                     case 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54:
-                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов и %d минуты", hours, minutes);
+                        System.out.printf("Даня пытался заварить доширак и сделать из него гитару %d часов и %d минуты\n", hours, minutes);
                         break;
                 }
             }
@@ -95,45 +97,42 @@ public class Mine {
 
         double length = Math.abs(Math.sqrt((Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2) + Math.pow((z2 - z1), 2)));
         String answer = decimalFormat.format(length);
-        System.out.println(answer);
+        System.out.println("Расстояние между кораблями равно "+answer);
     }
 
     public static void menu() {
+        System.out.println("Введите команду для выполнения:");
+        if (command != "skip") {
+                scanner.nextLine();
+        }
+        command = scanner.nextLine();
 
-        boolean status = false;
-        System.out.println("Введите номер задачи которую хотите решить:");
-        String command = scanner.nextLine();
         if (command.equals("20")) {
             task20();
-            status = true;
-            menu();
         }
-        if (command.equals("22")) {
+        else if (command.equals("22")) {
             task22();
-            status = true;
-            menu();
         }
-        if (command.equals("23")) {
+        else if (command.equals("23")) {
             task23();
-            status = true;
-            menu();
         }
-        if (command.equals("exit") | command.equals("Exit")) {
+        else if (command.equals("/exit")) {
             System.exit(228);
         }
-        if (status == false) {
-            System.out.println("Введите корректное значение!!!!!!!!!!!");
-            menu();
+        else if((command.equals("/help"))){
+            System.out.println("Доступные команды:\n20 - задача №20\n22 - задача №22\n23 - задача №23\n/exit - для выхода");
+            command = "skip";
         }
+        else {
+            System.out.println("Введите корректное значение!");
+            command = "skip";
+        }
+        menu();
     }
 
     public static void main(String[] args) {
-
-        //menu();
-
-        task20();
-        //task22();
-        //task23();
+        System.out.println("Введите /help для просмотра списка доступных команд");
+        menu();
     }
 
 }
